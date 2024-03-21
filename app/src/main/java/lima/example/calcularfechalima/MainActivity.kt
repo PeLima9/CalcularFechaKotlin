7358f528b8849cefa02c75b61d11ccce99e7453e
PeLima9 <eduardolima102a15@gmail.com>
2024-03-21 15:38:40 -0600

    Agregar y llamar elementos

--- app/src/main/java/lima/example/calcularfechalima/MainActivity.kt
+++ app/src/main/java/lima/example/calcularfechalima/MainActivity.kt
@@ -1,10 +1,14 @@
 package lima.example.calcularfechalima
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val dtpFecha = findViewById<CalendarView>(R.id.dtpFecha)
+        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+        val lblEdad = findViewById<TextView>(R.id.lblEdad)
+
+        
     }
 }
